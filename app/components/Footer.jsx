28f998037b72b33ebcc9b64dbfee9106f4c269c7
Christian Rodrigues <christianrodrigues02@gmail.com>
2024-03-22 11:23:18 -0700

    footer error fixed

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span>
-        <image src="/images/logo.png" alt="logo" width={50} height={50} />
+        <img src="/images/logo.png" alt="logo" width={60} height={60} />
         </span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
