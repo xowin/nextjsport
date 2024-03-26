0452445c06c2c4b2877992a27d8b88623202adea
Christian Rodrigues <christianrodrigues02@gmail.com>
2024-03-26 15:09:43 -0700

    img issue

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -61,7 +61,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[320px] lg:h-[320px] relative">
             <Image
-              src="/images/hero-img.jpg"
+              src="/images/hero-img.JPG"
               alt="hero-image"
               className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
