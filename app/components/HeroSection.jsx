38bad3eba93199c83fab0cc74bfaa5cc4f011472
Christian Rodrigues <christianrodrigues02@gmail.com>
2024-03-26 14:25:06 -0700

    apos

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-600">
-              Hello I'm <br></br>
+              Hello I&apos;m <br></br>
             </span>
             <TypeAnimation
               sequence={[
