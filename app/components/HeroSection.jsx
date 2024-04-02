e141ce14ba75fd3326ac7b7b805f9003edec0ce0
Christian Rodrigues <christianrodrigues02@gmail.com>
2024-04-02 16:14:45 -0700

    resume update

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -44,7 +44,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/Files/Resume-Christian-Rodrigues.pdf"
+              href="/Files/resume.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-600 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#373737] hover:bg-slate-800 rounded-full px-5 py-2">
